a55fb37429a61425f9c8d2b2d36f6adc50f4ee77
Alim Abay <abaev.alim@yandex.ru>
2019-01-15 22:15:19 +0300

    Added showing notification

--- app/src/main/java/com/example/adygha/locationchecker/MainActivity.java
+++ app/src/main/java/com/example/adygha/locationchecker/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.adygha.locationchecker;
 
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public int notificationId=111;
+    public String CHANNEL_ID="Main channel";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("Test title")
+                .setContentText("Test content")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(notificationId, mBuilder.build());
+
+
+//        Intent i= new Intent(this, AppService.class);
+        // potentially add data to the intent
+//        i.putExtra("KEY1", "Value to be used by the service");
+//        this.startService(i);
+
     }
 }
