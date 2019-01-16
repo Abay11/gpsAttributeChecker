bb12e5229cb3fe7b9ec9b85c1670dfd2462777e6
Alim Abay <abaev.alim@yandex.ru>
2019-01-16 22:24:20 +0300

    added implementation of checking attributes in dir

--- app/src/main/java/com/example/adygha/locationchecker/MainActivity.java
+++ app/src/main/java/com/example/adygha/locationchecker/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.adygha.locationchecker;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ExifInterface;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +14,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     static String APP_NAME="Location attribute checker";
@@ -53,6 +62,28 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "The service stopped", Toast.LENGTH_SHORT).show();
     }
 
+    public void checkPhotosClicked(View view)
+    {
+        Log.d(APP_NAME, "Check clicked");
+        String dir="sdcard/PhotoFolder/";
+
+//        File file = lastFileModified(path);
+        ArrayList<String> notTagged=new ArrayList<>();
+        File[] files=getFileList(dir);
+
+
+        for(File f : files)        {
+            {
+                if(!hasLocationTag(f.getAbsolutePath()))
+                    notTagged.add(f.getName());
+            }
+        }
+
+        System.out.println("Not tagged\n");
+        for (String f: notTagged)
+            System.out.println(f);
+    }
+
     public boolean isServiceRunning()
     {
         ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
@@ -70,5 +101,46 @@ public class MainActivity extends AppCompatActivity {
         Button stop=findViewById(R.id.stopButton);
         start.setEnabled(!state);
         stop.setEnabled(state);
+    }
+
+    public static File lastFileModified(String dir) {
+        File[] files = getFileList(dir);
+
+        long lastMod = Long.MIN_VALUE;
+        File choice = null;
+        for (File file : files) {
+            if (file.lastModified() > lastMod) {
+                choice = file;
+                lastMod = file.lastModified();
+            }
+        }
+        return choice;
+    }
+
+    public static File[] getFileList(String dir)
+    {
+        File fl = new File(dir);
+        File[] files = fl.listFiles(new FileFilter() {
+            public boolean accept(File file) {
+                return file.isFile();
+            }
+        });
+
+        return files;
+    }
+
+    public boolean hasLocationTag(String fileName)
+    {
+        try {
+            ExifInterface exifInterface = new ExifInterface(fileName);
+            String latitude=exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
+            if (latitude==null)
+                return false;
+            else return true;
+        }catch (IOException e)
+        {
+            Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show();
+            return false;
+        }
     }
 }
